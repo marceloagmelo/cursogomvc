e6a91fbed1e02e6fb4ba698f8f2df053a0e751b2
Marcelo Melo <marceloagmelo@gmail.com>
2019-06-23 10:58:40 -0300

    Ajustes

---
+++ routers/routes.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"net/http"
+	"github.com/labstack/echo"
+)
+
+//App
+var App *echo.Echo
+
+func init() {
+	App = echo.New()
+
+	App.GET("/", home)
+}
+
+func home(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
